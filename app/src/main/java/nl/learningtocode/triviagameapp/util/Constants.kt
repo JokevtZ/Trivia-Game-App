9a4444084e11d81c69b113a9ef2f0cf5064b0464
jokelugtigheid <jokevtz@gmail.com>
2022-01-22 22:40:19 +0100

    added right https adres

--- app/src/main/java/nl/learningtocode/triviagameapp/util/Constants.kt
+++ app/src/main/java/nl/learningtocode/triviagameapp/util/Constants.kt
@@ -1,5 +1,5 @@
 package nl.learningtocode.triviagameapp.util
 
 object Constants {
-    const val BASE_URL = "https://gist.githubusercontent.com/JokevtZ/83e205aa369b64002a1eb6e9b751f08c/raw/62e53d0587529b247f1c97b2160b5769c3897950/"
+    const val BASE_URL = "https://gist.githubusercontent.com/JokevtZ/83e205aa369b64002a1eb6e9b751f08c/raw/dc6aa46c7e9a790d0e798a3f08d1b6d0a83c012c/"
 }
