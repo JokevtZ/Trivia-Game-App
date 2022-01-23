980e0b375f6aa096d9f25cf57755688c44c89fec
jokelugtigheid <jokevtz@gmail.com>
2022-01-23 16:35:43 +0100

    added progress bar

--- app/src/main/java/nl/learningtocode/triviagameapp/component/Questions.kt
+++ app/src/main/java/nl/learningtocode/triviagameapp/component/Questions.kt
@@ -1,13 +1,17 @@
 package nl.learningtocode.triviagameapp.component
 
 import android.util.Log
+import android.view.RoundedCorner
+import android.widget.ProgressBar
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.RadioButton
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -22,6 +26,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -106,6 +111,7 @@ fun QuestionDisplay(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start)
         {
+            if (questionIndex.value >= 2) ShowProgress(score = questionIndex.value)
         QuestionTracker(counter = questionIndex.value)
             DottedLine(pathEffect = pathEffect)
 
@@ -216,6 +222,75 @@ fun DottedLine(pathEffect: PathEffect){
 
 }
 
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12){
+
+    val gradient = Brush.linearGradient(
+        listOf(
+            Color(0xFFF95075),
+            Color(0xFFBE6BE5
+            )
+        )
+    )
+
+    val progressFactor by remember(score){
+        mutableStateOf(score * 0.005f)
+
+    }
+
+    Row(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(45.dp)
+        .border(
+            width = 4.dp,
+            brush = Brush.linearGradient(
+                colors = listOf(
+                    AppColors.mLightPurple, AppColors.mLightPurple
+                )
+            ),
+            shape = RoundedCornerShape(34.dp)
+        )
+        .clip(
+            RoundedCornerShape(
+                topStartPercent = 50,
+                topEndPercent = 50,
+                bottomStartPercent = 50,
+                bottomEndPercent = 50
+            )
+        )
+        .background(Color.Transparent),
+        verticalAlignment = Alignment.CenterVertically)
+    {
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(brush = gradient),
+            enabled = false,
+            elevation = null,
+            colors = buttonColors(
+                backgroundColor = Color.Transparent,
+                disabledBackgroundColor = Color.Transparent
+            )
+        )
+        {
+            Text(text = (score * 10).toString(),
+            modifier = Modifier
+                .clip(
+                    shape = RoundedCornerShape(23.dp))
+                .fillMaxHeight(0.87f)
+                .fillMaxWidth()
+                .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center
+            )
+
+        }
+    }
+}
 
 @Preview
 @Composable
